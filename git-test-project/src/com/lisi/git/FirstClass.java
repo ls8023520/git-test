a9637fe80e16ceb8f7f6ed9ccf27678bdb8d60f6
lisi <1525050343@qq.com>
2020-08-02 10:37:10 +0800

    本地工程添加到Git

---
+++ git-test-project/src/com/lisi/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.lisi.git;
+
+/**
+ * @description:
+ * @author: 可爱的李四
+ * @time: 2020/8/2 10:20
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello giy project");
+        System.out.println("hello giy project");
+        System.out.println("hello giy project");
+        System.out.println("hello giy project");
+        System.out.println("hello giy project");
+    }
+
+}
